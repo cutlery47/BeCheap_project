fe639dcbe0fd12564676c1ea433c671517e361fd
Arkhip McNutt <ortiz47@mail.ru>
2023-07-28 14:49:08 +0400

    у фронта есть фронт -_-

--- BeCheap/frontend/be_cheap/src/Item.js
+++ BeCheap/frontend/be_cheap/src/Item.js
@@ -12,11 +12,13 @@ class Item extends React.Component{
         this.state = {
             item_data: []
         }
+
+        this.info = new Object
     }
 
     fetchData() {
         //берем данные с сервака и обноваляем данные для каждого Item
-        fetch('http://127.0.0.1:8000/api/v1/items/').then(response=>response.json())
+        fetch('http://127.0.0.1:8000/api/v1/items/').then(response=> response.json())
         .then((data)=>{
             this.setState({
                 item_data:data
@@ -32,34 +34,40 @@ class Item extends React.Component{
     //render - метод, возвращающий содержание React.Component-объекта (у нас это Item)
     //ну типо просто рендерит хуету
     render() {
-        
+      const allData = this.state.item_data;
+      let proper_objects = []
+
+      for (const data of allData) {
+        let info = new Object;
+        Object.assign(info, data);
+        proper_objects.push(info) 
+      }
+      
+      const rows=proper_objects.map((obj)=>
+      <div className='item'>
+          <div className = "item_image">
+               <img src={obj.item_image}/>
+         </div>
+          <div className="item_descript">
+          <span>
+              <b>Name:</b> {obj.item_name}
+            </span>
+            <span>
+              <b>Category:</b> {obj.item_category}
+            </span>
+            <span>
+              <b>Price:</b> {obj.item_cur_price} <strike> {obj.item_prev_price} </strike>
+            </span>
+          </div>
+      </div>
+      )
         return (
-            <div className = "item">
-                <style>
-                    <link rel="stylesheet" href="Item.css" type="text/css"/>
-                </style>
-              <div className = "item_image">
-                <img src="https://media.endclothing.com/media/f_auto,q_auto:eco,w_1600/prodmedia/media/catalog/product/1/0/10-03-23-TC_125BT222010F_1_1.jpg">
-                </img>
-              </div>
-              <div className = "item_descript">
-                <span className = "item_name">
-                  name
-                </span>
-                <span className = "item_color">
-                  color
-                </span>
-                <span className = "item_price_new">
-                  new price:
-                </span>
-                <span className = "item_price_prev">
-                  prev price:
-                </span>
-                <span className = "item_sale">
-                  sale:
-                </span>
-              </div>
-            </div>
+          <div class="clothing">
+            <style>
+               <link rel="stylesheet" href="Item.css" type="text/css"/>
+            </style>
+            {rows}
+          </div>
         );
     }
 }
